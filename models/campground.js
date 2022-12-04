aab55409eef0e8053371e5d7c99cd5e908677ee7
amaget8 <amaget8@gmail.com>
2022-12-04 14:18:54 +0900

    Seed Data + image

--- models/campground.js
+++ models/campground.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const CampgronundSchema = new Schema({
   title: String,
+  image: String,
   price: Number,
   location: String,
   description: String,
--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      image: "https://source.unsplash.com/user/larm",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem reiciendis asperiores, tempora voluptate repellendus iure! Quis aut veniam error, eveniet voluptatibus quidem asperiores, corporis facilis ipsa nulla repudiandae saepe! Aliquid!",
+      price: price,
     });
     await camp.save();
   }
