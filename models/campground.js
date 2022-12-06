65116cd46af6eeadd7f0b8a44c4c66981961093a
amaget8 <amaget8@gmail.com>
2022-12-06 16:00:19 +0900

    YelpCamp Review

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./reviews");
 const Schema = mongoose.Schema;
 
 const CampgronundSchema = new Schema({
@@ -7,6 +8,22 @@ const CampgronundSchema = new Schema({
   price: Number,
   location: String,
   description: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+});
+
+CampgronundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model("Campground", CampgronundSchema);
