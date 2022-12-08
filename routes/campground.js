a1268c3b052ad6e356642e3a2b967f2098bf2a3c
amaget8 <amaget8@gmail.com>
2022-12-08 18:17:51 +0900

    YelpCamp Register/Login/Logout

--- routes/campground.js
+++ routes/campground.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { campgroundSchema } = require("../schemas");
+const { isLoggedIn } = require("../middleware");
 const catchAsync = require("../utils/catchAsync");
 const ExpressError = require("../utils/ExpressError");
 const Campground = require("../models/campground");
@@ -23,12 +24,13 @@ router.get(
   })
 );
 
-router.get("/new", async (req, res) => {
+router.get("/new", isLoggedIn, async (req, res) => {
   res.render("campgrounds/new");
 });
 
 router.post(
   "/",
+  isLoggedIn,
   validateCampground,
   catchAsync(async (req, res) => {
     // if (!req.body.campground)
@@ -56,6 +58,7 @@ router.get(
 
 router.get(
   "/:id/edit",
+  isLoggedIn,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     if (!campground) {
@@ -68,6 +71,7 @@ router.get(
 
 router.put(
   "/:id",
+  isLoggedIn,
   validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
@@ -81,6 +85,7 @@ router.put(
 
 router.delete(
   "/:id",
+  isLoggedIn,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
---
+++ routes/users.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const catchAsync = require("../utils/catchAsync");
+const User = require("../models/user");
+
+router.get("/register", (req, res) => {
+  res.render("users/register");
+});
+
+router.post(
+  "/register",
+  catchAsync(async (req, res, next) => {
+    try {
+      const { username, email, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      req.login(registeredUser, (err) => {
+        if (err) return next(err);
+        req.flash("success", "Welcome to YelpCamp");
+        res.redirect("/campgrounds");
+      });
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("register");
+    }
+  })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+    keepSessionInfo: true,
+  }),
+  (req, res) => {
+    req.flash("success", "Welcome Back");
+    const redirectUrl = req.session.returnTo || "/campgrounds";
+    res.redirect(redirectUrl);
+  }
+);
+
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) next(err);
+    req.flash("success", "Goodbye😊");
+    delete req.session.returnTo;
+    res.redirect("/campgrounds");
+  });
+});
+
+module.exports = router;
